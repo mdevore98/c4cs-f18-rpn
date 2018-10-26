d7101ddf82702cd819674141c2bf2085ebdc5190
Madi <devore@umich.edu>
2018-10-26 15:01:49 -0400

    adding updated to rpn.py w tokens and such

--- rpn.py
+++ rpn.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-def caluclate(arg):
-    pass
+def calculate(arg):
+    stack = []
+
+    tokens = arg.split()
+
+    for token in tokens:
+        try:
+            stack.append(int(token))
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
 
 def main():
     while True:
