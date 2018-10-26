ad20db6777d0aaf620496e9818c440367aed2a82
Madi <devore@umich.edu>
2018-10-26 15:06:26 -0400

    Implement subtract (-)

--- rpn.py
+++ rpn.py
@@ -11,7 +11,13 @@ def calculate(arg):
         except ValueError:
             val1 = stack.pop()
             val2 = stack.pop()
-            result = val1 + val2
+	    if token == '+':
+                result = val1 + val2
+	    elif token == '-':
+		result = val1 - val2
+	    stack.append(result)
+    return stack[0]
+
 
 def main():
     while True:
