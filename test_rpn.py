6e2e84b973eb2b14e43786d3fb37425581a02989
Madi <devore@umich.edu>
2018-11-04 21:10:22 -0500

    fixed indentation errors

--- test_rpn.py
+++ test_rpn.py
@@ -3,17 +3,17 @@ import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        result = rpn.calculate('1 1 +')	
+        result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
     def test_sub(self):
         result = rpn.calculate('4 3 -')
-	self.assertEqual(1,result)
+        self.assertEqual(1,result)
     def test_multiply(self):
-	result = rpn.calculate('5 3 *')
-	self.assertEqual(15,result)
-   def test_divide(self):
-	result = rpn.calculate('6 3 /')
-	self.assertEqual(2,result)
+        result = rpn.calculate('5 3 *')
+        self.assertEqual(15,result)
+    def test_divide(self):
+        result = rpn.calculate('6 3 /')
+        self.assertEqual(2,result)
     def test_carat(self):
-	result = rpn.calculate('2 2 ^')
-	self.assertEqual(4,result)
+        result = rpn.calculate('2 2 ^')
+        self.assertEqual(4,result)
