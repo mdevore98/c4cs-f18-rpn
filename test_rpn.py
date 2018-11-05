deca2b9cd5ebfe5ae9fe3011f839851cd30ac8a7
Madi <devore@umich.edu>
2018-11-04 22:11:38 -0500

    trying to fix test_rpn.py .... again

--- test_rpn.py
+++ test_rpn.py
@@ -1,19 +1,20 @@
 import unittest
+
 import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        result = rpn.calculate('1 1 +')
+        result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
-    def test_sub(self):
-        result = rpn.calculate('4 3 -')
-        self.assertEqual(1,result)
+    def test_subtract(self):
+        result = rpn.calculate("5 3 -")
+        self.assertEqual(2, result)
     def test_multiply(self):
-        result = rpn.calculate('5 3 *')
-        self.assertEqual(15,result)
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
     def test_divide(self):
-        result = rpn.calculate('6 3 /')
-        self.assertEqual(2,result)
-    def test_carat(self):
-        result = rpn.calculate('2 2 ^')
-        self.assertEqual(4,result)
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
+    def test_power(self):
+        result = rpn.calculate("2 2 ^")
+        self.assertEqual(4, result)
